a1ae1b6d9e1cd61951c0c0227ad85c5c5443f312
Jiashun Zhou <zhoujiashun8@gmail.com>
2018-02-08 22:32:53 -0800

    add MainActivityTest

---
+++ app/src/androidTest/java/com/example/jiashunz/mobilecalllog/MainActivityTest.java
@@ -0,0 +1,40 @@
+package com.example.jiashunz.mobilecalllog;
+
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zhoujiashun on 2/8/18.
+ */
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    private MainActivity mActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void testLaunch() {
+        View view = mActivity.findViewById(R.id.call_recycler_view);
+
+        assertNotNull(view);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+    }
+
+}
